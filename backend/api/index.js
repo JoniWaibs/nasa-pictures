25e56aaf9eb98e17eb9842807c4f48987a038538
JoniWaibs <joniwaibs@gmail.com>
2022-01-30 13:53:36 -0300

    update: sintax

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,13 +1,9 @@
 const restClient = require('../configs/restClient');
 
-const request = {
-  api: (url, searchParams = {}) => {
-    const params = {};
-    params.sol = searchParams?.sol ?? 1000;
-    params.page = searchParams?.page ?? 1;
+module.exports = (url, searchParams = {}) => {
+  const params = {};
+  params.sol = searchParams?.sol ?? 1000;
+  params.page = searchParams?.page ?? 1;
 
-    return restClient.get(url, { params });
-  },
+  return restClient.get(url, { params });
 };
-
-module.exports = request;
